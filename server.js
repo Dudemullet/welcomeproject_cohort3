b05cc352b651338cfd93dffa0587b97a628e76d1
Dudemullet <elpaip@gmail.com>
2014-11-15 16:04:35 -0800

    1st commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var cors = require('express-cors')
+
+app.use(cors({
+  allowedOrigins: [
+    'mks.io'
+  ]
+}))
+
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
+
+var config = {
+  port: process.env.PORT || 8080
+};
+
+io.on('connection',function(socket) {
+
+  console.log("a user connected");
+
+  socket.on('disconnect', function() {
+    console.log("user disconnected");
+  });
+
+  socket.on('chat message', function(msg) {
+    console.log("msg: " + msg);
+    io.to(msg.room).emit('receive message', msg);
+  });
+
+  socket.on('join room', function(room){
+    socket.join(room);
+    console.log("a user joined room " + room);
+  });
+})
+
+http.listen(config.port, function(){
+  console.log('listening on *:' + config.port);
+});
